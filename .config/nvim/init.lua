7bf31a85f7c0f9ce800debb271d27ebc006466c6
maxwill <wupengboy72@gmail.com>
2024-02-26 11:22:10 +0800

    add zshrc nvim yay yarn configuration

---
+++ .config/nvim/init.lua
@@ -0,0 +1,32 @@
+vim.wo.number = true
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+end
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+	{import = "plugins"},
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+        },
+	{
+	"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+	}
+
+})
+
